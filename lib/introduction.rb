a5af25496a4ecdd828005a7c2f7192f11eddb2e6
Rachel Bennett <rachellaurynbennett@gmail.com>
2020-07-08 01:12:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language (name, name)
-  puts "Hi, my name is #{name, name}."
+def introduction_with_language (x, y)
+  puts "Hi, my name is #{x, y}."
 end
